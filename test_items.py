9f84df905cdb2c2d0a058f9bbb2d0af3a595c0b8
wasteland_rider <wasteland.rider.7bf@gmail.com>
2019-07-17 22:31:24 +0300

    Exclude sleep from test code.

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ def test_add_to_basket_exists(browser):
 	browser.get(link)
 	
 	# Uncomment next line to pause test.
-	time.sleep(30)
+	#time.sleep(30)
 	
 	button = browser.find_elements_by_css_selector('button.btn-add-to-basket')
 	assert len(button) == 1, f'{len(button)} buttons was found. Should be exactly 1 button.'
